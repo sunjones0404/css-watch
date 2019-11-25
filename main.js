2e051de5063d23d75cc1e48150ff8bbe81f65004
David Jones <djones7@Administrators-MacBook-Pro.local>
2019-11-25 17:38:17 +0000

    Minute hand added

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 const secondHandWrapper = document.querySelector('.hands-second-wrapper');
+const minuteHandWrapper = document.querySelector('.hands-minute-wrapper');
 
 let secondInterval = 6;
-
 setInterval(() => {
   secondInterval += 6;
   secondHandWrapper.style.transform = `rotate(${secondInterval}deg)`;
 }, 1000);
 
+let minuteInterval = 6;
+setInterval(() => {
+  minuteInterval += 6;
+  minuteHandWrapper.style.transform = `rotate(${secondInterval}deg)`;
+}, 1000);
+
 const day = document.querySelector('.calendar-day');
 const date = document.querySelector('.calendar-date');
-
 const weekday = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
 day.innerText = weekday[new Date().getDay()];
-
 date.innerText = new Date().getDate();
