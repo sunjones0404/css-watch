a595202b57cb7c0b8199cd9f6308505136f162cf
David Jones <davidjones1@unknown.home>
2020-01-28 11:26:10 +0000

    Face styling additions

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const secondHandWrapper = document.querySelector('.hands-second-wrapper');
 const minuteHandWrapper = document.querySelector('.hands-minute-wrapper');
 
-let secondInterval = 6;
+let secondInterval = 0;
 setInterval(() => {
   secondInterval += 6;
   secondHandWrapper.style.transform = `rotate(${secondInterval}deg)`;
 }, 1000);
 
-let minuteInterval = 6;
+let minuteInterval = 0;
 setInterval(() => {
   minuteInterval += 6;
-  minuteHandWrapper.style.transform = `rotate(${secondInterval}deg)`;
-}, 1000);
+  minuteHandWrapper.style.transform = `rotate(${minuteInterval}deg)`;
+}, 60000);
 
 const day = document.querySelector('.calendar-day');
 const date = document.querySelector('.calendar-date');
